2cc8ff673b7b7ed36b47801297cb355984c305ef
ひかり <68059597+hikarilife@users.noreply.github.com>
2021-05-11 09:30:40 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Noto Sans CJK JPの読み込み
+(function(d) {
+  var config = {
+    kitId: 'ldt1aps',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
+// Hamburger Menu
+$(function(){
+  $('#header__hamburger-btn').on('click',function(){
+      $(this).toggleClass('--active');
+      $('#header__nav').toggleClass('--active');
+  });
+});
